319eab380cac441c40310889a5575fc7359901f9
chxxah <chxxxah@gmail.com>
2023-11-19 16:51:07 +0900

    final

--- src/main/java/com/drhome/main/MainController.java
+++ src/main/java/com/drhome/main/MainController.java
@@ -23,52 +23,6 @@ public class MainController {
 	@Autowired
 	SearchService searchService;
 
-	@GetMapping(value = { "/", "/main" })
-	public String main(Model model, HttpSession session) {
-		int randomNumber = (int) (Math.random() * 30) + 1;
-		Map<String, Object> todayQuiz = mainService.findQuizByRandom(randomNumber);
-		List<Map<String, Object>> newQna = mainService.findNewQna();
-		if (session.getAttribute("mno") != null && session.getAttribute("mno") != "") {
-			List<Map<String, Object>> notification = mainService.getNotification(session.getAttribute("mno"));
-			Map<String, Object> countNotification = mainService.countNotification(session.getAttribute("mno"));
-			model.addAttribute("notification", notification);
-			model.addAttribute("countNotification", countNotification);
-		}
-		
-		// 진료과별 모달
-		List<Map<String, Object>> departmentKeyword = searchService.departmentKeyword();
-		model.addAttribute("departmentKeyword", departmentKeyword);
-
-		model.addAttribute("newQna", newQna);
-		model.addAttribute("quiz", todayQuiz);
-		return "/main";
-	}
 	
-	
-	
-	@PostMapping("/point")
-	public String point(@RequestParam Map<String, Object> data) {
-		mainService.raisePointByQuiz(data);
-		return "redirect:/main";
-	}
-
-	
-	@ResponseBody
-	@PostMapping("updateNotificationNum")
-	public String updateNotificationNum(@RequestParam("nno") int nno) {
-		mainService.updateNotificationNum(nno);
-
-		return "";
-	}
-	
-	@GetMapping("/menu")
-	public String menu(HttpSession session, Model model) {
-		if ( session.getAttribute("mno") != null && session.getAttribute("mno") != "") {
-			Map<String, Object> userInfo = mainService.userInfo(session.getAttribute("mno"));
-			model.addAttribute("userInfo", userInfo);
-			System.out.println(userInfo);
-		}
-		return "/menu";
-	}
 
 }
